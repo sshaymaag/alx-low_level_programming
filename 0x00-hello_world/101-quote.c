8cbe1e28b4f938d683bc243e0b26ffe8bfdf7bcd
sshaymaag <sshaymaag@hotmail.com>
2023-09-07 19:29:44 +0100

    #

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 	const char message[] =
 
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t written = write(2, message, sizeof(message) - 1);
+	write(2, message, sizeof(message) - 1);
 	return (1);
 }
 
