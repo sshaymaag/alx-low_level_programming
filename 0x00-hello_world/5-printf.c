238e3863de5149a02c5096f474341617b1d7a1c6
sshaymaag <sshaymaag@hotmail.com>
2023-09-07 18:48:16 +0100

    #

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print with proper grammar, but the outcome is a piece of art
+ *
+ * Return: 0 to quit
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
