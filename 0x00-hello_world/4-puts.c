8bc1daa35220150cb66e6922ea0714d7c6dfff28
sshaymaag <sshaymaag@hotmail.com>
2023-09-07 18:40:34 +0100

    #

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - hello world
+ *
+ * Return: 0 to quit
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
