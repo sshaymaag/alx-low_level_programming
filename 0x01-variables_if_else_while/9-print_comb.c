96219f52049c5e9e11cb48528531e325d3955b3e
sshaymaag <sshaymaag@hotmail.com>
2023-09-08 18:08:11 +0100

    #

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
