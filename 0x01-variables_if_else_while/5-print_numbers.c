365d6d0ed920c72758560a5a168b0d692726a984
sshaymaag <sshaymaag@hotmail.com>
2023-09-08 17:29:54 +0100

    #

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
