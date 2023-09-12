b75417aef1cf3f14160643ca4f212310fda0421e
sshaymaag <sshaymaag@hotmail.com>
2023-09-12 23:08:59 +0100

    #

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 
-void print_alphabet(void);
-
-/**
- * main - alphabets
- *
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
-
 /**
  * print_alphabet - alphabets
  */
