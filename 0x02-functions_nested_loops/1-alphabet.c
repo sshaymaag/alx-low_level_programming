be1514aafa1b91fb70bac8f44edf76ee37899ef8
sshaymaag <sshaymaag@hotmail.com>
2023-09-12 22:51:21 +0100

    #

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+void print_alphabet(void);
+
+/**
+ * main - alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets[26];
+
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+}
