eebeb756ee9d84ec29fe87df4a485c4aafb227a0
sshaymaag <sshaymaag@hotmail.com>
2023-09-13 22:04:46 +0100

    #

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int n) ;
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,20 +2,20 @@
 /**
 * positive_or_negative - if statement
 *
-*  @n: The character to print
+*  @i: The character to print
 */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 }
