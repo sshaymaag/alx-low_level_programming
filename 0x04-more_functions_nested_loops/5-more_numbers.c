25b322daf2ddc33bb1c585e284747501cb9a9565
sshaymaag <sshaymaag@hotmail.com>
2023-09-14 23:06:24 +0100

    #

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,14 +18,12 @@ void more_numbers(void)
 			if (i > 9)
 			{
 				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
 			}
 			else
 			{
-				_putchar('0' + i);
+				_putchar('0' + i % 10);
 			}
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
