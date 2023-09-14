6bb56e961185bc26f40f37f433ca3b9698eb7c31
sshaymaag <sshaymaag@hotmail.com>
2023-09-14 22:42:01 +0100

    #

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: 1 if n > 0;
+ * 0 if n = 0;
+ * -1 if n < 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
