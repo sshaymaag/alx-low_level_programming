976c819a0189362e01064dea6bd684d05c442dfc
sshaymaag <sshaymaag@hotmail.com>
2023-09-14 23:13:34 +0100

    #

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Return: 1 if n > 0;
+ * 0 if n = 0;
+ * -1 if n < 0
+ * @n : parameter
+ */
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j <= n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
