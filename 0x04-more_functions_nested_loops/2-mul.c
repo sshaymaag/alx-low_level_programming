5682147e58d0e9fb38a5d67742003e8833572789
sshaymaag <sshaymaag@hotmail.com>
2023-09-14 22:29:46 +0100

    #

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - is upper
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: 1 if n > 0;
+ * 0 if n = 0;
+ * -1 if n < 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
