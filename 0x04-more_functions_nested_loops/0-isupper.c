6a8f99d86ef27684c9df065939f4341ff9fdf712
sshaymaag <sshaymaag@hotmail.com>
2023-09-14 22:21:28 +0100

    #

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if n > 0;
+ * 0 if n = 0;
+ * -1 if n < 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
