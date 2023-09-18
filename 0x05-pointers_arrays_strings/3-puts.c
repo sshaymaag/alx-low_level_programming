350f06478365c65f72201e6c6210466224f667af
sshaymaag <sshaymaag@hotmail.com>
2023-09-18 23:09:44 +0100

    #

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - updates the value
+ * @str: argument
+ *
+ */
+void _puts(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len])
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar ('\n');
+}
