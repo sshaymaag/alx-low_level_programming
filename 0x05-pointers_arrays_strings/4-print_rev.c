f90653dc580d5278051b7a0a36f0de184f31b037
sshaymaag <sshaymaag@hotmail.com>
2023-09-18 23:14:28 +0100

    #

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_rev - updates the value
+ * @s: argument
+ *
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+	{
+		len++;
+	}
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar ('\n');
+}
