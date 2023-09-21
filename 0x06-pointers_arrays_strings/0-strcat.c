d6d8937397620d16b7bef7bb30816575b43ec2f2
sshaymaag <sshaymaag@hotmail.com>
2023-09-21 20:28:18 +0100

    #

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _strcat - writes the character c to stdout
+ * @dest: The character to print
+ * @src: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest,src));
+	return (strcat(dest, src));
 }
