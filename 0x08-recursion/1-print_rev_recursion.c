7e1ef89a9ed6070a6120c62cb14629efd3168c4c
sshaymaag <sshaymaag@hotmail.com>
2023-09-28 20:16:05 +0100

    #

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function that prints a string, followed by a new line
+ * @s: The string to print
+ *
+ * Return: nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	s++;
+	_print_rev_recursion(s);
+	putchar(*s);
+	}
+}
