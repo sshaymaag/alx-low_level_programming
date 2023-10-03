6e4d474800c112fc0efebf208969deb79d26e3ed
sshaymaag <sshaymaag@hotmail.com>
2023-10-03 01:56:47 +0100

    #

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,8 +9,13 @@
  * Return: nothing.
  */
 
-int main(int argc  __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
