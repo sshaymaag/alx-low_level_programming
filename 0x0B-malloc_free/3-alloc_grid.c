9dee6105d40ab08e3a069a42919dbb886717d33f
sshaymaag <sshaymaag@hotmail.com>
2023-10-03 21:46:44 +0100

    #

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,6 +23,18 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		s[i] = malloc(sizeof(int *) * width);
+
+		if (s[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(s[i]);
+
+			free(s);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width ; j++)
 		{
 			s[i][j] = 0;
