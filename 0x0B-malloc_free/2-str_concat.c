61617666e7c93ae91a57654b9f135d62438dcd92
sshaymaag <sshaymaag@hotmail.com>
2023-10-03 21:05:41 +0100

    #

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - function that prints a string, followed by a new line
+ * @s1: The string to print
+ * @s2: The string to print
+ *
+ * Return: nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	unsigned int i;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(char) * (strlen(s1) + strlen(s1) + 2));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		s[i + strlen(s1)] = s2[i];
+	}
+		return (s);
+}
