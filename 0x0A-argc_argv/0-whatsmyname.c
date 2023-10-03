bfdeb439f98ace1cb9ff456ac01712e4cbe55cee
sshaymaag <sshaymaag@hotmail.com>
2023-10-03 01:44:00 +0100

    #

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main (int argc __attribute__((unused)), char *argv[])
+/**
+ * main - function that prints a string, followed by a new line
+ * @argv: The string to print
+ * @argc: The string to print
+ *
+ * Return: nothing.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",*argv);
-	
-	return 0;
+	printf("%s\n", *argv);
+	return (0);
 }
