920ccae84bdf90e78d65f415375bd576821f5558
sshaymaag <sshaymaag@hotmail.com>
2023-10-03 13:53:57 +0100

    #

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * _strdup - function that prints a string, followed by a new line
+ * @str: The string to print
+ *
+ * Return: nothing.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(sizeof(str));
+	for (i = 0; str[i] != '\0'; i++)
+		s[i] = str[i];
+	return (s);
+}
