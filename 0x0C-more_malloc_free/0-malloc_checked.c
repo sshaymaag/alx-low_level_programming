8adfc4ab13014879097ffd40b6db1f163f66b8b4
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 00:10:49 +0100

    #

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - function that prints a string, followed by a new line
+ * @b: The string to print
+ *
+ * Return: nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
