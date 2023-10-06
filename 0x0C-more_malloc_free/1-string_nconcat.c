7893d5cf333518f3fda94248c6aba60cd48c4e50
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 00:47:05 +0100

    #

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = 0; j < n && s2[j] != '\0'; j++)
+	for (j = 0; j < n; j++)
 	{
 		ptr[i] = s2[j];
 		i++;
