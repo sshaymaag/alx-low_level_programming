7a59490008f410bd507a463c1ecc8d18632c99f4
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 01:16:11 +0100

    #

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,8 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s2[j];
 		i++;
 	}
-	while (n >= strlen(s2) && i < (strlen(s1) + strlen(s2)))
-		ptr[i++] = s2[j++];
-	
+
+	ptr[i] = '\0';
 	return (ptr);
 }
