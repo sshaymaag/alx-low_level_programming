0228c848206a3ff266f68b6c4fcd24c9ca03e44d
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 12:07:41 +0100

    #

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,14 +3,34 @@
 #include <stdio.h>
 
 /**
- * string_nconcat - function that prints a string, followed by a new line
- * @s1: The string to print
- * @s2: The string to print
- * @n: The string to print
+ * array_range - function that prints a string, followed by a new line
+ * @min: The string to print
+ * @max: The string to print
  *
  * Return: nothing.
  */
 int *array_range(int min, int max)
 {
-	
+	int *ptr;
+	int i, j = 0, len = min;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	while (len <= max)
+	{
+		len++;
+	}
+	ptr = malloc(sizeof(int) * len + 1);
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		ptr[j] = i;
+		j++;
+	}
+	return (ptr);
 }
