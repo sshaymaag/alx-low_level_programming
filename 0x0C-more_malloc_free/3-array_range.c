0b18887b68454af14af94b0a0130563f464ca2ce
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 01:58:35 +0100

    #

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - function that prints a string, followed by a new line
+ * @s1: The string to print
+ * @s2: The string to print
+ * @n: The string to print
+ *
+ * Return: nothing.
+ */
+int *array_range(int min, int max)
+{
+	
+}
