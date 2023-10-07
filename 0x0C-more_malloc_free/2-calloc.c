a4dc102568effdb61587f645d86b9fc4c7c03f2b
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 01:52:24 +0100

    #

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: char to copy
+ * @n: n time b copied
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 /**
  * _calloc - function that prints a string, followed by a new line
  * @nmemb: The string to print
@@ -12,7 +30,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	
+
 	if (size == 0 || nmemb == 0)
 	{
 		return (NULL);
@@ -23,5 +41,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	return(ptr);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
