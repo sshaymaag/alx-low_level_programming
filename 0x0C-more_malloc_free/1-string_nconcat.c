8281530abbac1e439ab0428180601ffe600e408e
sshaymaag <sshaymaag@hotmail.com>
2023-10-07 01:14:42 +0100

    #

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,6 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n >= strlen(s2) && i < (strlen(s1) + strlen(s2)))
 		ptr[i++] = s2[j++];
-	ptr[i] = '\0';
+	
 	return (ptr);
 }
