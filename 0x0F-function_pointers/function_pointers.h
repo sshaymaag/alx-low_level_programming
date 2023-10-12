849f01b156a8022d6a78b07ede89a5918d06e4f8
sshaymaag <sshaymaag@hotmail.com>
2023-10-12 02:08:31 +0100

    #

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+#include <stddef.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
