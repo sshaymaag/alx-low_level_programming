ed51987f21c258ef5d856bd4721970daadf37c1c
root <root@ThinkPad>
2023-10-19 22:54:09 +0100

    5

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,11 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void beforeMain() __attribute__((constructor));
-void beforeMain() {
-    printMessage();
-}
-
 
 /**
  * struct list_s - singly linked list
@@ -33,6 +28,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_first(void);
 
 
 #endif
