ea4bf810f4f64f56883f296edb3b1635e47f526e
root <root@ThinkPad>
2023-10-30 21:16:21 +0100

    0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
